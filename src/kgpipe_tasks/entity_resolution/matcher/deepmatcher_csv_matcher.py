926dc4b17750424d7430efc8469c14756f749c9f
Theo <gg46ixav@studserv.uni-leipzig.de>
2026-01-20 10:20:59 +0100

    cleaned deepmatcher_csv_matcher.py

--- src/kgpipe_tasks/entity_resolution/matcher/deepmatcher_csv_matcher.py
+++ src/kgpipe_tasks/entity_resolution/matcher/deepmatcher_csv_matcher.py
@@ -1,18 +1,13 @@
 """
-Paris RDF Matcher task implementation.
+Deepmatcher CSV Matcher task implementation.
 """
 
-from pathlib import Path
-from typing import Dict, Any
-import pandas as pd
-import os
-import csv
+from typing import Dict
 
 
-from kgpipe.common import KgTask, DataFormat, Data, Registry
+from kgpipe.common import DataFormat, Data, Registry
 from kgpipe.common.io import get_docker_volume_bindings, remap_data_path_for_container
 from kgpipe.execution import docker_client
-from kgpipe_tasks.transform_interop.exchange.entity_matching import ER_Match, ER_Document
 
 @Registry.task(
     input_spec={"data_dir": DataFormat.DIR, "train": DataFormat.CSV,  "valid": DataFormat.CSV,  "test": DataFormat.CSV,  "unlabeled": DataFormat.CSV},
@@ -56,103 +51,3 @@ def deepmatcher_entity_matching(inputs: Dict[str, Data], outputs: Dict[str, Data
     # Execute the container
     result = client()
     print(f"DeepMatcher entity matching completed: {result}")
-
-
-
-    
-PREFIX_MAP = {
-    "dbp": "http://dbpedia.org/",
-    "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-    "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "xsd" : "http://www.w3.org/2001/XMLSchema#",
-    "schema" : "http://schema.org/",
-    "dbo": "http://dbpedia.org/ontology/",
-    "foaf": "http://xmlns.com/foaf/0.1/",
-    "skos": "http://www.w3.org/2004/02/skos/core#",
-}
-
-def resolvePrefixedUri(uri):
-    if not uri.startswith("http://") and not uri.startswith("https://"):
-        prefix, suffix = uri.split(":", 1)
-        # try:
-        prefix = PREFIX_MAP[prefix]
-        # except Exception as e:
-        #     print(f"Unknown prefix: {prefix} for {uri}")
-        #     raise Exception(f"Unknown prefix: {prefix} for {uri}")
-        return prefix + suffix
-    else:
-        return uri
-
-
-@Registry.task(
-    input_spec={"input": DataFormat.PARIS_CSV},
-    output_spec={"output": DataFormat.ER_JSON},
-    description="Convert Paris CSV output to standard RDF matching format"
-)
-def paris_exchange(inputs: Dict[str, Data], outputs: Dict[str, Data]):
-    """
-    Convert Paris CSV output to standard RDF matching format.
-    
-    Args:
-        inputs: Dictionary mapping input names to Data objects (Paris CSV)
-        outputs: Dictionary mapping output names to Data objects (RDF)
-    """
-    print(f"Converting Paris CSV to matching format with inputs: {inputs}")
-
-    input_path = inputs["input"].path
-    output_path = outputs["output"].path
-    
-    Path(output_path).touch()
-    files = [str(f) for f in os.listdir(input_path)]
-
-    iteration_ids = [ int(f.split("_")[0]) for f in files if f.endswith(".tsv") ]
-
-    iteration_ids.sort()
-
-    last_eqv_it = iteration_ids[-1]
-
-    def getEqvFileName(id): return f"{id}_eqv.tsv"
-    def getRelFileNames(id): return [f"{id}_superrelations1.tsv",f"{id}_superrelations2.tsv"]
-
-    def check_file_exists(last_eqv_it):
-        try:
-            return os.stat(os.path.join(input_path, getEqvFileName(last_eqv_it))).st_size > 0
-        except FileNotFoundError:
-            return -1
-
-    while 0 == check_file_exists(last_eqv_it) :
-        last_eqv_it -= 1
-
-    last_relation_it = last_eqv_it - 1
-
-    def extract_matches(file,id_type):
-        with open(file, newline='', encoding='utf-8') as csvfile:
-            reader = csv.reader(csvfile, delimiter='\t')
-            for row in reader:
-                if len(row) == 3:
-                    er_match = ER_Match(
-                        id_1=resolvePrefixedUri(row[0]),
-                        id_2=resolvePrefixedUri(row[1]),
-                        score=float(row[2]),
-                        id_type=id_type
-                    )
-                    matches.append(er_match)
-
-    matches = []
-
-    if last_eqv_it == -1:
-        doc = ER_Document(matches=[])
-        with open(outputs["output"].path, 'w', encoding='utf-8') as jsonfile:
-            jsonfile.write(doc.model_dump_json())
-    else:
-        eqv_file = getEqvFileName(last_eqv_it)
-        rel_files = getRelFileNames(last_relation_it)
-
-        extract_matches(os.path.join(input_path,eqv_file),"entity")
-        [ extract_matches(os.path.join(input_path,f), "relation") for f in rel_files ]
-
-        doc = ER_Document(matches=matches)
-
-        with open(outputs["output"].path, 'w', encoding='utf-8') as jsonfile:
-            jsonfile.write(doc.model_dump_json())
-
